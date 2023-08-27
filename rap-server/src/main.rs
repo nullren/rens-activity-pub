6319ee6b84ac8e0c8124c1b93b53df3ba7e44551
Renning Bruns <ren@renmail.net>
2023-08-27 15:08:08 -0700

    cli parse port

--- rap-server/src/main.rs
+++ rap-server/src/main.rs
@@ -3,6 +3,7 @@ use axum::{
     response::Json,
     Router,
 };
+use clap::Parser;
 use serde_json::{Value, json};
 
 // `&'static str` becomes a `200 OK` with `content-type: text/plain; charset=utf-8`
@@ -16,14 +17,28 @@ async fn json() -> Json<Value> {
     Json(json!({ "data": 42 }))
 }
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Address to listen on
+    #[arg(short, long, default_value = "0.0.0.0")]
+    address: String,
+
+    /// Port to listen on
+    #[arg(short, long, default_value = "3000")]
+    port: String,
+}
+
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+
     let app = Router::new()
         .route("/plain_text", get(plain_text))
         .route("/json", get(json));
 
-    // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let addr = format!("{}:{}", cli.address, cli.port).parse().unwrap();
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
