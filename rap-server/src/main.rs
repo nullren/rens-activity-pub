e2ff699449afb8e17a839ef6a0acfdf872739eb0
Renning Bruns <ren@renmail.net>
2023-08-27 14:59:22 -0700

    simple axum server started

--- rap-server/src/main.rs
+++ rap-server/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    response::Json,
+    Router,
+};
+use serde_json::{Value, json};
+
+// `&'static str` becomes a `200 OK` with `content-type: text/plain; charset=utf-8`
+async fn plain_text() -> &'static str {
+    "foo"
+}
+
+// `Json` gives a content-type of `application/json` and works with any type
+// that implements `serde::Serialize`
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/plain_text", get(plain_text))
+        .route("/json", get(json));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
