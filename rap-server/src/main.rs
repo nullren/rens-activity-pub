8fe62cdccc157bd5f9794d0f32679720865f479c
Renning Bruns <ren@renmail.net>
2023-09-02 12:58:58 -0700

    add a simple request logger

--- rap-server/src/main.rs
+++ rap-server/src/main.rs
@@ -1,11 +1,11 @@
-use axum::{
-    routing::get,
-    response::Json,
-    Router,
-};
+use axum::{routing::get, response::Json, Router, middleware};
+use axum::http::{Request, StatusCode};
+use axum::middleware::Next;
+use axum::response::Response;
 use axum_prometheus::PrometheusMetricLayerBuilder;
 use clap::Parser;
 use serde_json::{Value, json};
+use tower::ServiceBuilder;
 
 // `&'static str` becomes a `200 OK` with `content-type: text/plain; charset=utf-8`
 async fn plain_text() -> &'static str {
@@ -45,7 +45,11 @@ async fn main() {
         .route("/plain_text", get(plain_text))
         .route("/json", get(json))
         .route("/metrics", get(|| async move { metric_handle.render() }))
-        .layer(prometheus_layer);
+        .layer(
+            ServiceBuilder::new()
+                .layer(prometheus_layer)
+                .layer(middleware::from_fn(request_logger))
+        );
 
     let addr = format!("{}:{}", cli.address, cli.port);
     log::info!("Listening on {}", addr);
@@ -56,3 +60,29 @@ async fn main() {
         .await
         .unwrap();
 }
+
+async fn request_logger<B>(
+    // you can also add more extractors here but the last
+    // extractor must implement `FromRequest` which
+    // `Request` does
+    request: Request<B>,
+    next: Next<B>,
+) -> Result<Response, StatusCode> {
+    let start = std::time::Instant::now();
+    let method = request.method().clone();
+    let uri = request.uri().clone();
+    let path = uri.path().clone();
+    let query = uri.query().unwrap_or("");
+    let response = next.run(request).await;
+    // log request and response details here
+    log::info!(
+        "{} {} {} {} {}ms",
+        response.status(),
+        method,
+        path,
+        query,
+        start.elapsed().as_millis()
+    );
+    Ok(response)
+}
+
