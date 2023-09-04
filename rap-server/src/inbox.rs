ce63c5cfacb280018d7454d1bc53988b0a661870
Renning Bruns <ren@renmail.net>
2023-09-03 17:36:55 -0700

    rebuild sig str correctly

--- rap-server/src/inbox.rs
+++ rap-server/src/inbox.rs
@@ -1,5 +1,4 @@
 use crate::config::Config;
-use crate::key;
 use crate::signature::Signature;
 use crate::users::{PeopleStore, PersonId};
 use axum::extract::Path;
@@ -31,7 +30,7 @@ pub async fn json(
         )
     })?;
 
-    verify_signature(&headers, signature, person.key)?;
+    let _comparison = rebuild_sig_str(&actor, &headers, &signature);
 
     // let date = chrono::Utc::now().to_rfc2822();
     Ok(Json(json!("OK")))
@@ -47,10 +46,61 @@ fn header_str<'a>(headers: &'a HeaderMap, name: &str) -> Result<&'a str, (Status
         })
 }
 
-fn verify_signature(
-    _headers: &HeaderMap,
-    _signature: Signature,
-    _key: key::Key,
-) -> Result<(), (StatusCode, String)> {
-    Ok(())
+fn rebuild_sig_str(
+    account: &PersonId,
+    headers: &HeaderMap,
+    signature: &Signature,
+) -> String {
+    signature
+        .headers
+        .iter()
+        .map(|header| {
+            if header == "(request-target)" {
+                format!("(request-target): post /users/{}/inbox", account)
+            } else {
+                let header = header.to_lowercase();
+                let value = header_str(headers, &header).unwrap_or("");
+                format!("{}: {}", header, value)
+            }
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
+}
+
+#[cfg(test)]
+mod tests {
+    use axum::http::HeaderValue;
+    use super::*;
+
+    #[test]
+    fn test_rebuild_sig_str() {
+        // Create a mock HeaderMap
+        let mut headers = HeaderMap::new();
+        headers.insert("Host", HeaderValue::from_static("example.com"));
+        headers.insert("Date", HeaderValue::from_static("Sun, 06 Nov 2021 08:49:37 GMT"));
+
+        // Create a mock Signature
+        let signature = Signature {
+            key_id: "".to_string(),
+            headers: vec![
+                String::from("(request-target)"),
+                String::from("Host"),
+                String::from("Date"),
+            ],
+            // Add other fields if the Signature struct has more
+            signature: "".to_string(),
+        };
+
+        // Create a mock PersonId
+        let person_id = "alice".to_string();
+
+        // Call the function
+        let result = rebuild_sig_str(&person_id, &headers, &signature);
+
+        // Expected signature string
+        let expected_result = "(request-target): post /users/alice/inbox\nhost: example.com\ndate: Sun, 06 Nov 2021 08:49:37 GMT";
+
+        assert_eq!(result, expected_result, "Rebuilt signature string did not match expected string");
+    }
+
 }
